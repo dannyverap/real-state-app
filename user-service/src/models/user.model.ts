2d80fd618434a9440527adf39c90296d79b1ac99
Danny Vera <danny.verap@gmail.com>
2024-03-17 19:00:24 +0100

    Create User Model

---
+++ user-service/src/models/user.model.ts
@@ -0,0 +1,17 @@
+import { Role } from "@prisma/client"
+
+export class User {
+    constructor(
+        public readonly phone: string,
+        public readonly mail: string,
+        public readonly password: string,
+        public readonly salt: string,
+        public readonly user_type: Role,
+        public readonly verification_code: number | null,
+        public readonly expiry: Date | null,
+        public readonly verified: boolean,
+        public readonly first_name: string | null,
+        public readonly last_name: string | null,
+        public readonly id?: number,
+    ) { }
+}
