9685b3d7bc29efdae83b3719f51cf6593dadba07
Danny Vera <danny.verap@gmail.com>
2024-03-19 23:07:44 +0100

    Create DBOperation class to handle operations

---
+++ client-service/src/repository/dbOperation.ts
@@ -0,0 +1,13 @@
+import { DBClient } from "../utils/dataBaseClient";
+
+export class DBOperation {
+    constructor() { }
+
+    async executeQuery(queryString: string, values: unknown[]) {
+        const client = DBClient()
+        await client.connect()
+        const result = await client.query(queryString, values)
+        await client.end()
+        return result
+    }
+}
