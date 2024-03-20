324182b41c0b9c6429c4f34796d5c60214988722
Danny Vera <danny.verap@gmail.com>
2024-03-20 10:41:08 +0100

    Update clienteService

--- client-service/src/service/clientService.ts
+++ client-service/src/service/clientService.ts
@@ -11,4 +11,24 @@ export class ClientService {
         const data = await this._repository.CreateClient(input)
         return data
     }
+    async GetClients(limit: number, offset: number) {
+        const data = await this._repository.FindClients(limit, offset)
+        return data
+    }
+    async GetClientById(client_id: number) {
+        const data = await this._repository.FindClientById(client_id)
+        return data
+    }
+    async GetClientByNationalId(national_id: string) {
+        const data = await this._repository.FindClientByNationalId(national_id)
+        return data
+    }
+    async GetClientByMail(mail_id: string) {
+        const data = await this._repository.FindClientByMail(mail_id)
+        return data
+    }
+    async UpdateClient(id: number, input: any) {
+        const data = await this._repository.UpdateClient(id, input)
+        return data
+    }
 }
