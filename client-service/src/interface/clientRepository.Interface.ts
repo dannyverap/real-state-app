e5fba0e298076c8e4b2bdb2058bad3cdc57c06a7
Danny Vera <danny.verap@gmail.com>
2024-03-20 10:40:42 +0100

    Update repository interface

--- client-service/src/interface/clientRepository.Interface.ts
+++ client-service/src/interface/clientRepository.Interface.ts
@@ -2,7 +2,9 @@ import { ClientModel } from "../models/ClientModel";
 
 export interface IClientRepository {
     CreateClient(data: ClientModel): Promise<ClientModel>;
-    FindClient(mail: string): Promise<ClientModel>;
-    FindClientById(id: number): Promise<ClientModel>;
+    FindClientById(client_id: number): Promise<ClientModel>;
+    FindClientByNationalId(national_id: string): Promise<ClientModel>;
+    FindClientByMail(mail: string): Promise<ClientModel>;
+    FindClients(limit: number, offset: number): Promise<ClientModel[]>;
     UpdateClient(id: number, data: ClientModel): Promise<ClientModel>;
 }
